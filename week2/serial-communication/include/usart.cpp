9a0dfb6d2c0807f06392426688a6724bf540fb89
bytecod3 <emwiti658@gmail.com>
2022-05-20 17:02:07 +0300

    compilation success

--- week2/serial-communication/include/usart.cpp
+++ week2/serial-communication/include/usart.cpp
@@ -16,7 +16,26 @@ void initUSART(){ /* requires baud. defined in setbaud.h*/
     UCSR0B = (1 << TXEN0) | (RXEN0); // enable TX and RX
 
     UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // define data length. 8 data bits, 1 stop bit
-    
+
+}
+
+void transmitByte(int data){
+    loop_until_bit_is_set(UCSR0A, UDRE0); // wait for empty transmit buffer
+    UDR0 = data;
+}
+
+int receiveByte(){
+    loop_until_bit_is_set(UCSR0A, RXC0); // wait for incoming data
+    return UDR0; // return register value
+}
+
+void printString(const char myString[]){
+    int i = 0;
+    while(myString[i]){
+        transmitByte(myString[i]);
+        i++;
+    }
+
 }
 
 
